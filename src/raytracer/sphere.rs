2c87c3e16aced0126656ba0bc7d25dd9891b6b89
Dobiasd <editgym@gmail.com>
2020-05-18 08:22:59 +0200

    Make hit method of sphere

--- src/raytracer/sphere.rs
+++ src/raytracer/sphere.rs
@@ -10,6 +10,48 @@ pub struct Sphere {
     pub material: Material,
 }
 
+impl Sphere {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<Hit> {
+        let oc = r.origin - &self.center;
+        let a = r.direction.length_squared();
+        let half_b = dot(&oc, &r.direction);
+        let c = oc.length_squared() - self.radius * self.radius;
+        let discriminant = half_b * half_b - a * c;
+
+        if discriminant > 0.0 {
+            let root = discriminant.sqrt();
+            let temp = (-half_b - root) / a;
+            // todo: remove duplication of these two blocks
+            if temp < t_max && temp > t_min {
+                let p = r.at(temp);
+                let outward_normal = (p - &self.center) / self.radius;
+                let (front_face, normal) = face_normal(r, &outward_normal);
+                return Some(Hit {
+                    position: p,
+                    t: temp,
+                    normal,
+                    front_face,
+                    material: self.material,
+                });
+            }
+            let temp2 = (-half_b + root) / a;
+            if temp2 < t_max && temp2 > t_min {
+                let p = r.at(temp2);
+                let outward_normal = (p - &self.center) / self.radius;
+                let (front_face, normal) = face_normal(r, &outward_normal);
+                return Some(Hit {
+                    position: p,
+                    t: temp2,
+                    normal,
+                    front_face,
+                    material: self.material,
+                });
+            }
+        }
+        return None;
+    }
+}
+
 pub struct SphereWorld {
     pub spheres: Vec<Sphere>
 }
@@ -19,7 +61,7 @@ impl SphereWorld {
         let mut closest_so_far = t_max;
         let mut rec: Option<Hit> = None;
         for sphere in &self.spheres {
-            match hit_sphere(&sphere.center, sphere.radius, r, &sphere.material, t_min, closest_so_far) {
+            match sphere.hit(r, t_min, closest_so_far) {
                 Some(temp_rec) => {
                     closest_so_far = temp_rec.t;
                     rec = Some(temp_rec);
@@ -29,45 +71,4 @@ impl SphereWorld {
         }
         return rec;
     }
-}
-
-// todo: make method
-fn hit_sphere(center: &Vector3d, radius: f64, r: &Ray, mat: &Material, t_min: f64, t_max: f64) -> Option<Hit> {
-    let oc = r.origin - center;
-    let a = r.direction.length_squared();
-    let half_b = dot(&oc, &r.direction);
-    let c = oc.length_squared() - radius * radius;
-    let discriminant = half_b * half_b - a * c;
-
-    if discriminant > 0.0 {
-        let root = discriminant.sqrt();
-        let temp = (-half_b - root) / a;
-        // todo: remove duplication of these two blocks
-        if temp < t_max && temp > t_min {
-            let p = r.at(temp);
-            let outward_normal = (p - center) / radius;
-            let (front_face, normal) = face_normal(r, &outward_normal);
-            return Some(Hit {
-                position: p,
-                t: temp,
-                normal,
-                front_face,
-                material: *mat,
-            });
-        }
-        let temp2 = (-half_b + root) / a;
-        if temp2 < t_max && temp2 > t_min {
-            let p = r.at(temp2);
-            let outward_normal = (p - center) / radius;
-            let (front_face, normal) = face_normal(r, &outward_normal);
-            return Some(Hit {
-                position: p,
-                t: temp2,
-                normal,
-                front_face,
-                material: *mat,
-            });
-        }
-    }
-    return None;
 }
