c98b61d0c559e21660081137f5cdae9d29dec253
Dobiasd <editgym@gmail.com>
2020-05-18 15:18:16 +0200

    Improve error message

--- src/raytracer/image.rs
+++ src/raytracer/image.rs
@@ -17,19 +17,22 @@ impl Image {
 
     pub fn save_png(&self, path: &Path) {
         println!("Saving {}", path.display());
-        let file = File::create(path).unwrap();
+        let file = File::create(path)
+            .expect(&format!("Unable to create file {}", path.display()));
         let mut encoder =
             png::Encoder::new(BufWriter::new(file),
                               self.width as u32, self.height as u32);
         encoder.set_color(png::ColorType::RGB);
         encoder.set_depth(png::BitDepth::Eight);
-        let mut writer = encoder.write_header().unwrap();
+        let mut writer = encoder.write_header()
+            .expect(&format!("Unable to create file writer"));
         let png_data = (0..self.height).map(|y| {
             (0..self.width).map(|x| {
                 let c = self.get(x, self.height - y - 1).to_canvas_color();
                 vec![c.r, c.g, c.b]
             }).collect::<Vec<Vec<u8>>>().concat()
         }).collect::<Vec<Vec<u8>>>().concat();
-        writer.write_image_data(&png_data).unwrap();
+        writer.write_image_data(&png_data)
+            .expect(&format!("Unable to write file {}", path.display()));
     }
 }
