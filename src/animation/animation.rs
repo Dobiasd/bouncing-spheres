5a93d409588b467d7b9fe3e3d8b821add9a8519f
Dobiasd <editgym@gmail.com>
2020-05-23 05:57:26 +0200

    Split animation and video code from main

---
+++ src/animation/animation.rs
@@ -0,0 +1,95 @@
+use std::f64::consts::PI;
+use std::ops::{Add, Div, Mul, Sub};
+
+use rand::prelude::StdRng;
+use rand::Rng;
+use uuid::Uuid;
+
+use crate::raytracer::camera::Camera;
+use crate::raytracer::color::Color;
+use crate::raytracer::material::Material;
+use crate::raytracer::sphere::Sphere;
+use crate::raytracer::vector3d::Vector3d;
+use crate::raytracer::world::World;
+
+fn random_sphere(rng: &mut StdRng) -> Sphere {
+    let min = -5.0;
+    let max = 5.0;
+    let max_start_y = 123.0;
+    let radius = 0.4 + 1.7 *
+        rng.gen_range(-10.0_f64, 0.9_f64).tanh().add(1.0).div(2.0);
+    let center = Vector3d {
+        x: rng.gen_range(min, max),
+        y: rng.gen_range(radius, max_start_y),
+        z: rng.gen_range(min, max),
+    };
+    Sphere {
+        id: Uuid::new_v4(),
+        center,
+        radius,
+        material: Material {
+            albedo: Color {
+                r: rng.gen_range(0.0, 1.0),
+                g: rng.gen_range(0.0, 1.0),
+                b: rng.gen_range(0.0, 1.0),
+            },
+            reflectiveness: 1.0 + 0.0 * rng.gen_range(0.0, 1.0),
+            reflection_fuzz: 0.0 + 0.0 * rng.gen_range(0.0, 1.0),
+        },
+        speed: Vector3d { x: 0.0, y: 0.0, z: 0.0 },
+        mass: radius.powf(3.0),
+        extra_brightness: 0.0,
+        center_old: center,
+    }
+}
+
+pub fn make_world(rng: &mut StdRng) -> World {
+    let radius_planet = 6371.0;
+    let center = Vector3d { x: 0.0, y: -radius_planet, z: 0.0 };
+    let planet = Sphere {
+        id: Uuid::new_v4(),
+        center,
+        radius: radius_planet,
+        material: Material {
+            albedo: Color { r: 0.5, g: 0.5, b: 0.5 },
+            reflectiveness: 0.75,
+            reflection_fuzz: 0.08,
+        },
+        speed: Vector3d { x: 0.0, y: 0.0, z: 0.0 },
+        mass: radius_planet.powf(3.0),
+        extra_brightness: 0.0,
+        center_old: center,
+    };
+
+    let number_of_spheres = 80;
+    World {
+        spheres: (0..number_of_spheres).map(move |_| random_sphere(rng))
+            .chain(std::iter::once(planet))
+            .collect()
+    }
+}
+
+pub fn cam(width: usize, height: usize, t_real: f64) -> Camera {
+    let t_cam = t_real.mul(5.0).sub(2.3).tanh().add(1.0).div(2.0);
+    let position = Vector3d {
+        x: 15.0 * (7.1 * t_cam).sin(),
+        y: 0.1 + 8.1 * t_cam.mul(-1.0).add(1.0),
+        z: 15.0 * (5.9 * (t_cam - 0.05)).cos(),
+    };
+    let looks_at = Vector3d {
+        x: 0.3 * (7.1 * t_cam).sin(),
+        y: position.y.sqrt().div(4.0),
+        z: 0.3 * (8.1 * t_cam).cos(),
+    };
+    let v_rotation = t_real.mul(40.0).sub(20.0).tanh().add(1.0).mul(PI);
+    let up_direction = Vector3d { x: 0.0, y: v_rotation.cos(), z: v_rotation.sin() };
+    let dist_to_looks_at = (position - &looks_at).length();
+    let dist_to_focus = (dist_to_looks_at + 0.1 * (0.74 * t_cam).sin()).max(3.5);
+    let max_aperture = 0.17;
+    let aperture = max_aperture - t_real.powf(5.0) * max_aperture;
+    let aspect_ratio = width as f64 / height as f64;
+    let vertical_field_of_view = 80.0;
+
+    Camera::new(&position, &looks_at, &up_direction, vertical_field_of_view,
+                aspect_ratio, aperture, dist_to_focus)
+}
