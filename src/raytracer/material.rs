41652994cff94824fb96bb5e088ebdf8f8407744
Dobiasd <editgym@gmail.com>
2020-05-23 13:52:18 +0200

    Refactor scattering

--- src/raytracer/material.rs
+++ src/raytracer/material.rs
@@ -15,27 +15,37 @@ pub struct Material {
 
 impl Material {
     #[inline(always)]
-    pub fn scatter(&self, rng: &mut StdRng, ray: &Ray, rec: &Hit) -> Option<(Ray, Color)> {
-        if rng.gen::<f64>() > self.reflectiveness {
-            let scatter_direction = rec.normal + &random_unit_vector(rng);
-            let scattered = Ray {
-                origin: rec.position,
-                direction: scatter_direction,
-                frame_time: ray.frame_time,
-            };
+    fn scatter_lambertian(&self, rng: &mut StdRng, ray: &Ray, rec: &Hit) -> Option<(Ray, Color)> {
+        let scatter_direction = rec.normal + &random_unit_vector(rng);
+        let scattered = Ray {
+            origin: rec.position,
+            direction: scatter_direction,
+            frame_time: ray.frame_time,
+        };
+        Some((scattered, self.albedo))
+    }
+
+    #[inline(always)]
+    fn scatter_reflective(&self, rng: &mut StdRng, ray: &Ray, rec: &Hit) -> Option<(Ray, Color)> {
+        let reflected = reflect(&unit_vector(&ray.direction), &rec.normal);
+        let scattered = Ray {
+            origin: rec.position,
+            direction: reflected + &(random_unit_vector(rng) * self.reflection_fuzz),
+            frame_time: ray.frame_time,
+        };
+        if dot(&scattered.direction, &rec.normal) > 0.0 {
             Some((scattered, self.albedo))
         } else {
-            let reflected = reflect(&unit_vector(&ray.direction), &rec.normal);
-            let scattered = Ray {
-                origin: rec.position,
-                direction: reflected + &(random_unit_vector(rng) * self.reflection_fuzz),
-                frame_time: ray.frame_time,
-            };
-            if dot(&scattered.direction, &rec.normal) > 0.0 {
-                Some((scattered, self.albedo))
-            } else {
-                None
-            }
+            None
+        }
+    }
+
+    #[inline(always)]
+    pub fn scatter(&self, rng: &mut StdRng, ray: &Ray, rec: &Hit) -> Option<(Ray, Color)> {
+        if rng.gen::<f64>() > self.reflectiveness {
+            self.scatter_lambertian(rng, ray, rec)
+        } else {
+            self.scatter_reflective(rng, ray, rec)
         }
     }
 }
